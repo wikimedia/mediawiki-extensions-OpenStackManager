46f820e7449cbcca6ac20e225e7dc20e699c7754
Andrew Otto <acotto@gmail.com>
2012-12-13 16:44:32 -0500

    Now using real groups with real member lists for posixgroups for Projects.
    ProjectGroups are posixgroups with manually maintained member lists.
    Whenever a Project is created, deleted, or has members added or removed,
    the same action will be called on the ProjectGroup.  This keeps the
    Project and corresponding ProjectGroup member lists in sync.
    
    There is also a new maintenance script 'syncProjectGroups.php', that
    can be run to ensure that every Project's ProjectGroup member list
    matches the Project's.
    
    This allows applications that use LDAP to actually search for members
    of specific posix groups.
    
    Previously, ProjectGroups were maintained as virtual static groups,
    pointing at the Project.
    
    Change-Id: Idb33f2f48893a9afcfb4e008372386637c7a5bda

--- OpenStackNovaProject.php
+++ OpenStackNovaProject.php
@@ -13,6 +13,7 @@ class OpenStackNovaProject {
 	var $projectInfo;
 	var $roles;
 	var $loaded;
+	var $projectGroup;
 
 	// list of roles
 	static $rolenames = array( 'sysadmin', 'netadmin' );
@@ -50,7 +51,43 @@ class OpenStackNovaProject {
 		foreach ( self::$rolenames as $rolename ) {
 			$this->roles[] = OpenStackNovaRole::getProjectRoleByName( $rolename, $this );
 		}
+		// fetch the associated posix project group (project-$projectname)
+		$this->fetchProjectGroup();
+
 		$this->loaded = true;
+	}
+
+
+	/**
+	 * Initializes the corresponding project group object for this project.
+	 * If the ProjectGroup can't be loaded OR if the existing ProjectGroup
+	 * is a virtual static group, then the ProjectGroup will be recreated
+	 * from scratch and the members will be synced from this Project's
+	 * list of members.
+	 *
+	 * @return void
+	 */
+	function fetchProjectGroup() {
+		global $wgAuth;
+		$this->projectGroup = new OpenStackNovaProjectGroup( $this->projectname );
+
+		// If we couldn't find an corresponding Project Group,
+		// then we should create one now.
+		if ( !$this->projectGroup->loaded or $this->projectGroup->isVirtual() ) {
+			$wgAuth->printDebug( $this->projectGroup->getProjectGroupName() . " either does not exist or is a virtual static group.  Recreating is as a real group and syncing members.", NONSENSITIVE );
+
+			// Delete, recreate, and then sync the members.
+			$deleteSuccess = OpenStackNovaProjectGroup::deleteProjectGroup( $this->projectname );
+			// if we successfully deleted the ProjectGroup, then recreate it now.
+			if ( $deleteSuccess ) {
+				$createSuccess = OpenStackNovaProjectGroup::createProjectGroup( $this->projectname );
+				// Aaaaand if we successfully created the group, then finally sync the members from this project now.
+				if ( $createSuccess ) {
+					$this->projectGroup = new OpenStackNovaProjectGroup( $this->projectname );
+					$this->syncProjectGroupMembers();
+				}
+			}
+		}
 	}
 
 	/**
@@ -58,6 +95,20 @@ class OpenStackNovaProject {
 	 */
 	function getProjectName() {
 		return $this->projectname;
+	}
+
+
+	/**
+	 * Returns the corresponding ProjectGroup for this Project.
+	 * If necessary, the ProjectGroup will be loaded from LDAP.
+	 *
+	 * @return OpenStackNovaProjectGroup
+	 */
+	function getProjectGroup() {
+		if ( !$this->loaded ) {
+			$this->fetchProjectGroup();
+		}
+		return $this->projectGroup;
 	}
 
 	/**
@@ -79,6 +130,9 @@ class OpenStackNovaProject {
 		$members = array();
 		if ( isset( $this->projectInfo[0]['member'] ) ) {
 			$memberdns = $this->projectInfo[0]['member'];
+			// The first element in the member list is the count
+			// of entries in the list.  We don't want that!
+			// Shift it off.
 			array_shift( $memberdns );
 			foreach ( $memberdns as $memberdn ) {
 				$searchattr = $wgAuth->getConf( 'SearchAttribute' );
@@ -100,14 +154,26 @@ class OpenStackNovaProject {
 		return $members;
 	}
 
-	function getProjectDN() {
-		return $this->projectDN;
+	/**
+	 * Returns an array of all member DNs that belong to this project.
+	 *
+	 * @return array
+	 */
+	function getMemberDNs() {
+		$memberDNs = array();
+		if ( isset( $this->projectInfo[0]['member'] ) ) {
+			$memberDNs = $this->projectInfo[0]['member'];
+			// The first element in the member list is the count
+			// of entries in the list.  We don't want that!
+			// Shift it off.
+			array_shift( $memberDNs );
+			sort( $memberDNs );
+		}
+		return $memberDNs;
 	}
 
-	function getProjectGroupDN() {
-		global $wgOpenStackManagerLDAPProjectGroupBaseDN;
-
-		return 'cn=project-' . $this->getProjectName() . ',' . $wgOpenStackManagerLDAPProjectGroupBaseDN;
+	function getProjectDN() {
+		return $this->projectDN;
 	}
 
 	function getSudoersDN() {
@@ -128,12 +194,12 @@ class OpenStackNovaProject {
 			array_shift( $members );
 			$user = new OpenStackNovaUser( $username );
 			if ( ! $user->userDN ) {
-				$wgAuth->printDebug( "Failed to find userDN in deleteMember", NONSENSITIVE );
+				$wgAuth->printDebug( "Failed to find userDN for username $username in OpenStackNovaProject deleteMember", NONSENSITIVE );
 				return false;
 			}
 			$index = array_search( $user->userDN, $members );
 			if ( $index === false ) {
-				$wgAuth->printDebug( "Failed to find userDN in member list", NONSENSITIVE );
+				$wgAuth->printDebug( "Failed to find userDN " . $user->userDN . " in Project " . $this->projectname . " member list", NONSENSITIVE );
 				return false;
 			}
 			unset( $members[$index] );
@@ -142,8 +208,13 @@ class OpenStackNovaProject {
 			foreach ( $members as $member ) {
 				$values['member'][] = $member;
 			}
+
 			$success = LdapAuthenticationPlugin::ldap_modify( $wgAuth->ldapconn, $this->projectDN, $values );
 			if ( $success ) {
+				// If we successfully deleted the Project Member, then also
+				// delete the member from the corresponding ProjectGroup.
+				$this->projectGroup->deleteMember( $username );
+
 				foreach ( $this->roles as $role ) {
 					$role->deleteMember( $username );
 					# @todo Find a way to fail gracefully if role member
@@ -162,7 +233,7 @@ class OpenStackNovaProject {
 				$wgAuth->printDebug( "Successfully removed $user->userDN from $this->projectDN", NONSENSITIVE );
 				return true;
 			} else {
-				$wgAuth->printDebug( "Failed to remove $user->userDN from $this->projectDN", NONSENSITIVE );
+				$wgAuth->printDebug( "Failed to remove $user->userDN from $this->projectDN: " . ldap_error($wgAuth->ldapconn), NONSENSITIVE );
 				return false;
 			}
 		} else {
@@ -192,15 +263,62 @@ class OpenStackNovaProject {
 		$members[] = $user->userDN;
 		$values = array();
 		$values['member'] = $members;
+
 		$success = LdapAuthenticationPlugin::ldap_modify( $wgAuth->ldapconn, $this->projectDN, $values );
+
+
 		if ( $success ) {
-			$this->fetchProjectInfo(true);
+			// If we successfully added the member to this Project, then
+			// also add the member to the corresponding ProjectGroup.
+			$this->projectGroup->addMember( $username );
+
+			$this->fetchProjectInfo( true );
 			$wgAuth->printDebug( "Successfully added $user->userDN to $this->projectDN", NONSENSITIVE );
 			return true;
 		} else {
-			$wgAuth->printDebug( "Failed to add $user->userDN to $this->projectDN", NONSENSITIVE );
+			$wgAuth->printDebug( "Failed to add $user->userDN to $this->projectDN: " . ldap_error($wgAuth->ldapconn), NONSENSITIVE );
 			return false;
 		}
+	}
+
+	/**
+	 * Compares members between this Project and its
+	 * corresponding ProjectGroup.  If they differ,
+	 * Then the entire member list for the ProjectGroup
+	 * will be overwritten with this list of members.
+	 *
+	 * @return int -1 on failure, 0 on nochange, and 1 on a successful sync
+	 */
+	function syncProjectGroupMembers() {
+		$failure  = -1;
+		$nochange =  0;
+		$synced   =  1;
+
+		// if we're not using project groups, just return $nochange.
+		if ( !OpenStackNovaProject::useProjectGroup() ) {
+			$retval = $nochange;
+		}
+
+		else {
+			// These both return a sorted array of Member DNs
+			$projectMemberDNs      = $this->getMemberDNs();
+			$projectGroupMemberDNs = $this->projectGroup->getMemberDNs();
+
+			// These two arrays should be exactly the same, 
+			// so comparing them using == should work.
+			// If they are not the same, then modify the
+			// project group member list so that it exactly
+			// matches the list from the project.
+			if ( $projectMemberDNs != $projectGroupMemberDNs ) {
+				$sync_success = $this->projectGroup->setMembers( $projectMemberDNs );
+				$retval = $sync_success == true ? $synced : $failure;
+			}
+			else {
+				$retval = $nochange;
+			}
+		}
+
+		return $retval;
 	}
 
 	static function useProjectGroup() {
@@ -294,18 +412,9 @@ class OpenStackNovaProject {
 		$project['member'] = $wgOpenStackManagerLDAPUser;
 		$projectdn = 'cn=' . $projectname . ',' . $wgOpenStackManagerLDAPProjectBaseDN;
 
-		$projectGroup = array();
-		if ( OpenStackNovaProject::useProjectGroup() ) {
-			// Split posix group away from project name, prefixed by project
-			$projectGroupName = 'project-' . $projectname;
-			$projectGroup['objectclass'][] = 'posixgroup';
-			$projectGroup['objectclass'][] = 'groupofnames';
-			$projectGroup['objectclass'][] = 'ds-virtual-static-group';
-			$projectGroup['cn'] = $projectGroupName;
-			$projectGroup['gidnumber'] = OpenStackNovaUser::getNextIdNumber( $wgAuth, 'gidnumber' );
-			$projectGroup['ds-target-group-dn'] = $projectdn;
-			$projectGroupdn = 'cn=' . $projectGroupName . ',' . $wgOpenStackManagerLDAPProjectGroupBaseDN;
-		} else {
+		// if we're not going to use project groups, 
+		// then create this project as a posixgroup
+		if ( !OpenStackNovaProject::useProjectGroup() ) {
 			$project['gidnumber'] = OpenStackNovaUser::getNextIdNumber( $wgAuth, 'gidnumber' );
 			$project['objectclass'][] = 'posixgroup';
 		}
@@ -326,8 +435,12 @@ class OpenStackNovaProject {
 			LdapAuthenticationPlugin::ldap_add( $wgAuth->ldapconn, $sudoerOUdn, $sudoerOU );
 			# TODO: If sudoerOU creation fails we need to be able to fail gracefully
 			$wgAuth->printDebug( "Successfully added project $projectname", NONSENSITIVE );
-			if ( $projectGroup ) {
-				LdapAuthenticationPlugin::ldap_add( $wgAuth->ldapconn, $projectGroupdn, $projectGroup );
+
+			// Now that we've created the Project, if we
+			// are supposed to use a corresponding Project Group
+			// to manage posix group permissions, do so now.
+			if ( OpenStackNovaProject::useProjectGroup() ) {
+				OpenStackNovaProjectGroup::createProjectGroup( $projectname );
 				# TODO: If project group creation fails we need to be able to fail gracefully
 			}
 			return true;
@@ -367,16 +480,12 @@ class OpenStackNovaProject {
 				$wgAuth->printDebug( "Failed to delete role $roledn", NONSENSITIVE );
 			}
 		}
-		# Projects can have a separate group entry
+
+		# Projects can have a separate group entry.  If so, delete it now.
 		if ( OpenStackNovaProject::useProjectGroup() ) {
-			$groupdn = $project->getProjectGroupDN();
-			$success = LdapAuthenticationPlugin::ldap_delete( $wgAuth->ldapconn, $groupdn );
-			if ( $success ){
-				$wgAuth->printDebug( "Successfully deleted group $groupdn", NONSENSITIVE );
-			} else {
-				$wgAuth->printDebug( "Failed to delete group $groupdn", NONSENSITIVE );
-			}
+			OpenStackNovaProjectGroup::deleteProjectGroup( $projectname );
 		}
+
 		# Projects have a sudo OU and sudoers entries below that OU, we must delete them first
 		$sudoers = OpenStackNovaSudoer::getAllSudoersByProject( $project->getProjectName() );
 		foreach ( $sudoers as $sudoer ) {
