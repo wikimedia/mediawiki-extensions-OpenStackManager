812810cd7aa1747b23fd9dbf0400ddd01599b051
Paladox <thomasmulhall410@yahoo.com>
2016-03-11 21:00:09 +0000

    Add jshint to npm
    Change-Id: I8da5f300ee8201ab47233a665cd14ceafdef1804

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,19 @@
 /*jshint node:true */
 module.exports = function ( grunt ) {
-	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
 
 	grunt.initConfig( {
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'**/*.js',
+				'!node_modules/**'
+			]
+		},
 		banana: {
 			all: 'i18n/'
 		},
@@ -15,6 +25,6 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'jshint', 'jsonlint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
