7083edd8160306499381fc12032759815baf2cc6
andrewbogott <abogott@wikimedia.org>
2019-11-05 21:28:32 -0600

    Remove all special pages related to OpenStack
    Bug: T161553
    Change-Id: I7cfb4f89c7827426bf8c7306ab9e8dc94970be2a

--- special/SpecialNova.php
+++ special/SpecialNova.php
@@ -35,35 +35,12 @@ abstract class SpecialNova extends SpecialPage {
 	}
 
 	/**
-	 * @param string $project
-	 * @return void
-	 */
-	protected function notInProject( $project ) {
-		$this->setHeaders();
-		$this->getOutput()->setPageTitle( $this->msg( 'openstackmanager-noaccount', $project ) );
-		$this->getOutput()->addWikiMsg( 'openstackmanager-noaccount2', $project );
-	}
-
-	/**
 	 * @return void
 	 */
 	protected function notInServiceGroup() {
 		$this->setHeaders();
 		$this->getOutput()->setPageTitle( $this->msg( 'openstackmanager-notinservicegroup' ) );
 		$this->getOutput()->addWikiMsg( 'openstackmanager-needservicegroup' );
-	}
-
-	/**
-	 * @param string $role
-	 * @param string $project
-	 * @return void
-	 */
-	protected function notInRole( $role, $project ) {
-		$this->setHeaders();
-		$this->getOutput()->setPageTitle(
-			$this->msg( 'openstackmanager-needrole', $role, $project )
-		);
-		$this->getOutput()->addWikiMsg( 'openstackmanager-needrole2', $role, $project );
 	}
 
 	protected function checkTwoFactor() {
@@ -112,61 +89,6 @@ abstract class SpecialNova extends SpecialPage {
 		} else {
 			return true;
 		}
-	}
-
-	protected function getProjectFilter() {
-		global $wgRequest;
-
-		if ( $wgRequest->getCookie( 'projectfilter' ) ) {
-			return explode( ',', urldecode( $wgRequest->getCookie( 'projectfilter' ) ) );
-		}
-		return [];
-	}
-
-	protected function showProjectFilter( $projects ) {
-		if ( $this->getRequest()->wasPosted() &&
-			$this->getRequest()->getVal( 'action' ) !== 'setprojectfilter'
-		) {
-			return null;
-		}
-		$showmsg = $this->getRequest()->getText( 'showmsg' );
-		if ( $showmsg === "setfilter" ) {
-			$this->getOutput()->addWikiMsg( 'openstackmanager-setprojects' );
-		}
-		$currentProjects = $this->getProjectFilter();
-		$project_keys = [];
-		$defaults = [];
-		foreach ( $projects as $project ) {
-			$projectName = $project->getProjectName();
-			$project_keys[$projectName] = $projectName;
-			if ( in_array( $projectName, $currentProjects ) ) {
-				$defaults[$projectName] = $projectName;
-			}
-		}
-		$projectFilter = [];
-		$projectFilter['projects'] = [
-			'type' => 'multiselect',
-			'label-message' => 'openstackmanager-projects',
-			'section' => 'projectfilter',
-			'options' => $project_keys,
-			'default' => $defaults,
-			'dropdown' => true,
-			'name' => 'projects',
-		];
-		$projectFilter['action'] = [
-			'type' => 'hidden',
-			'default' => 'setprojectfilter',
-			'name' => 'action',
-		];
-		$projectFilterForm = new HTMLForm(
-			$projectFilter,
-			$this->getContext(),
-			'openstackmanager-novaprojectfilter'
-		);
-		$projectFilterForm->setSubmitID( 'novaproject-form-setprojectfiltersubmit' );
-		$projectFilterForm->setSubmitCallback( [ $this, 'trySetProjectFilter' ] );
-		$projectFilterForm->setSubmitTextMsg( 'openstackmanager-projectfiltersubmit' );
-		$projectFilterForm->show();
 	}
 
 	public static function createResourceLink( $resource ) {
@@ -239,90 +161,5 @@ abstract class SpecialNova extends SpecialPage {
 			$table .= Html::rawElement( 'tr', [], $rowOut );
 		}
 		return Html::rawElement( 'table', [ 'class' => 'wikitable sortable' ], $table );
-	}
-
-	/**
-	 * Create a project section to be displayed in a list page. Warning: neither $actionsByRole nor
-	 * $data escaped in this function and must be escaped prior to this call.
-	 *
-	 * @param string $projectName
-	 * @param array[] $actionsByRole
-	 * @param string $data
-	 *
-	 * @return string
-	 */
-	protected function createProjectSection( $projectName, $actionsByRole, $data ) {
-		$actions = [];
-		foreach ( $actionsByRole as $role => $roleActions ) {
-			foreach ( $roleActions as $action ) {
-				if ( !$role || $this->userLDAP->inRole( $role, $projectName ) ) {
-					$actions[] = $action;
-				}
-			}
-		}
-		if ( $actions ) {
-			$actions = implode( ', ', $actions );
-			$actions = "[$actions]";
-		} else {
-			$actions = "";
-		}
-		$actionOut = Html::rawElement( 'span', [ 'id' => 'novaaction' ], $actions );
-		$projectNameOut = $this->createResourceLink( $projectName );
-		# Mark this element with an id so that we can #link to it from elsewhere.
-		$out = Html::rawElement(
-			'h2',
-			[ 'id' => Sanitizer::escapeIdForAttribute( $projectName ) ],
-			$projectNameOut . ' ' . $actionOut
-		);
-		$out .= Html::rawElement( 'div', [], $data );
-
-		return $out;
-	}
-
-	/**
-	 * Create a region section to be displayed in a list page. Warning: neither $actionsByRole nor
-	 * $data are escaped in this function and must be escaped prior to this call.
-	 *
-	 * @param string $region
-	 * @param string $projectName
-	 * @param array $actionsByRole
-	 * @param string $data
-	 *
-	 * @return string
-	 */
-	protected function createRegionSection( $region, $projectName, $actionsByRole, $data ) {
-		$actions = [];
-		foreach ( $actionsByRole as $role => $roleActions ) {
-			foreach ( $roleActions as $action ) {
-				if ( $this->userLDAP->inRole( $role, $projectName ) ) {
-					$actions[] = $action;
-				}
-			}
-		}
-		$escapedregion = htmlentities( $region );
-		if ( $actions ) {
-			$actions = implode( ', ', $actions );
-			$actionOut = Html::rawElement( 'span', [ 'id' => 'novaaction' ], "[$actions]" );
-		} else {
-			$actionOut = '';
-		}
-		$out = Html::rawElement( 'h3', [], "$escapedregion $actionOut" );
-		$out .= Html::rawElement( 'div', [], $data );
-
-		return $out;
-	}
-
-	public function trySetProjectFilter( $formData, $entryPoint = 'internal' ) {
-		global $wgRequest;
-
-		if ( !$formData['projects'] ) {
-			$wgRequest->response()->setCookie( 'projectfilter', '', time() - 86400 );
-		} else {
-			$projects = implode( ',', $formData['projects'] );
-			$wgRequest->response()->setCookie( 'projectfilter', $projects );
-		}
-		$this->getOutput()->redirect( $this->getPageTitle()->getFullUrl() );
-
-		return true;
 	}
 }
