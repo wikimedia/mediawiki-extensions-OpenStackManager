1e112dbee5ad66e11f31ffd5f52fe460bc16ad5e
Aaron Schulz <aschulz@wikimedia.org>
2020-02-06 19:57:09 -0800

    Convert $wgMemc use to WANObjectCache
    This makes use of the changed cache key as per I3025517fccd.
    
    Bug: T160813
    Depends-On: I3025517fccd8d493ef89f678fee1d41471ac7b0b
    Change-Id: Ie9cc55d6d4fa2ae62c83d058c4b0547eb87d24b3

--- includes/OpenStackNovaUser.php
+++ includes/OpenStackNovaUser.php
@@ -4,6 +4,7 @@
  * @file
  * @ingroup Extensions
  */
+use MediaWiki\MediaWikiServices;
 
 class OpenStackNovaUser {
 
@@ -105,8 +106,6 @@ class OpenStackNovaUser {
 	 * @return bool
 	 */
 	public function importKeypair( $key ) {
-		global $wgMemc;
-
 		$ldap = LdapAuthenticationPlugin::getInstance();
 		$keypairs = [];
 		if ( isset( $this->userInfo[0]['sshpublickey'] ) ) {
@@ -119,9 +118,10 @@ class OpenStackNovaUser {
 		$success = LdapAuthenticationPlugin::ldap_modify( $ldap->ldapconn, $this->userDN, $values );
 		if ( $success ) {
 			$ldap->printDebug( "Successfully imported the user's sshpublickey", NONSENSITIVE );
-			$key = wfMemcKey( 'ldapauthentication', "userinfo", $this->userDN );
 			$ldap->printDebug( "Deleting memcache key: $key.", NONSENSITIVE );
-			$wgMemc->delete( $key );
+			// @TODO: don't depend on cache key naming internals of another extension
+			$cache = MediaWikiServices::getInstance()->getMainWANObjectCache();
+			$cache->delete( $cache->makeKey( 'ldapauthentication-userinfo', $this->userDN ) );
 			$this->fetchUserInfo();
 			return true;
 		} else {
@@ -135,8 +135,6 @@ class OpenStackNovaUser {
 	 * @return bool
 	 */
 	public function deleteKeypair( $key ) {
-		global $wgMemc;
-
 		$ldap = LdapAuthenticationPlugin::getInstance();
 		if ( isset( $this->userInfo[0]['sshpublickey'] ) ) {
 			$keypairs = $this->userInfo[0]['sshpublickey'];
@@ -157,9 +155,10 @@ class OpenStackNovaUser {
 			);
 			if ( $success ) {
 				$ldap->printDebug( "Successfully deleted the user's sshpublickey", NONSENSITIVE );
-				$key = wfMemcKey( 'ldapauthentication', "userinfo", $this->userDN );
 				$ldap->printDebug( "Deleting memcache key: $key.", NONSENSITIVE );
-				$wgMemc->delete( $key );
+				// @TODO: don't depend on cache key naming internals of another extension
+				$cache = MediaWikiServices::getInstance()->getMainWANObjectCache();
+				$cache->delete( $cache->makeKey( 'ldapauthentication-userinfo', $this->userDN ) );
 				$this->fetchUserInfo();
 				return true;
 			} else {
